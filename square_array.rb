76953f2e6cb706c89f933a5aea263e6eec3a652d
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 15:29:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,12 @@
 def square_array(array)
   # your code here
   array.each do |m|
-    output = m**2
+    puts m
+    output = m ** 2
     puts output
   end
-  
+
 end
+
+test_array = [1,2,3,4,8]
+square_array(test_array)
